3f6f56ac4926373f4260e3dbabf3e5afbc4ccbed
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-06-13 19:50:43 +1000

    34

---
+++ DZ34.py
@@ -0,0 +1,14 @@
+def ritm(text):
+    lst_1 = list(text.split())
+    vowels = set('уеаоэяиюыё')
+    for i in range(len(lst_1)):
+        count = 0
+        for letter in lst_1[i]:
+            if letter in vowels:
+                count += 1
+            lst_1[i] = count
+    if len(set(lst_1)) == 1:
+        return "Парам пам-пам"
+    return "Пам парам"
+text = 'парам пам-пам'
+print(ritm(text))
